dcaac2d77d9ffd585d95d3ca85768bf5123688d5
rkyzk <reikoyazaki@yahoo.com>
2024-03-30 15:17:32 +0900

    add func to upload & download imgs from aws in service class

--- src/main/java/natureblossom/ims/service/ImageUploadService.java
+++ src/main/java/natureblossom/ims/service/ImageUploadService.java
@@ -1,10 +1,17 @@
 package natureblossom.ims.service;
 
-import java.io.InputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 
 
@@ -15,12 +22,51 @@ import com.amazonaws.services.s3.model.S3Object;
  * @version 1.0.0
  */
 @Service
-public interface ImageUploadService {
-	public PutObjectResult upload(
-			String path,
-			String fileName,
-			InputStream inputStrem);
+public class ImageUploadService {
+	@Autowired
+	private AmazonS3 amazonS3;
 	
-	public S3Object download(String path, String fileName);
+	@Value("${aws.endpoint.url}")
+    private String endpoint;
 	
+	/*
+	 * Upload files to AWS S3 bucket.
+	 * 
+	 * @params multipart file, folder name, file name
+	 * @return image url
+	 */
+	public String uploadImg(MultipartFile multipartFile,
+			String folder, String fileName) {
+		String path = endpoint + "/" + folder;
+		String imgUrl = path + "/" + fileName;
+		try {
+			// convert multipart file to file.
+			File file = convertMultipartFileToFile(multipartFile);
+			// upload the file to specified path
+			amazonS3.putObject(new PutObjectRequest(path, fileName, file)
+				.withCannedAcl(CannedAccessControlList.PublicRead));
+			file.delete();		
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		return imgUrl;
+	}
+	
+	public S3Object downloadImg(String path, String fileName) {
+	        return amazonS3.getObject(path, fileName);
+	}
+	
+	/*
+	 * Convert multipart file to file
+	 * 
+	 * @params multipart file
+	 * @return File: converted file
+	 */
+	public File convertMultipartFileToFile(MultipartFile file) throws IOException {
+		File convertedFile = new File(file.getOriginalFilename());
+		FileOutputStream fos = new FileOutputStream(convertedFile);
+		fos.write(file.getBytes());
+		fos.close();		
+		return convertedFile;
+	}
 }
