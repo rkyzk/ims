dbaa2c5e9985f960672a2572cea9def8595b6691
rkyzk <reikoyazaki@yahoo.com>
2024-03-30 10:33:56 +0900

    add input form on prod-reg.html

--- src/main/java/natureblossom/ims/controller/ProductRegistrationController.java
+++ src/main/java/natureblossom/ims/controller/ProductRegistrationController.java
@@ -3,6 +3,9 @@ package natureblossom.ims.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import natureblossom.ims.entity.Product;
 
 /** Controller for product registration page.
  *
@@ -19,7 +22,8 @@ public class ProductRegistrationController {
 	 * @return product registration page
 	 */
 	@GetMapping("/product-registration")
-	public String getProductRegistration(Model model) {
+	public String getProductRegistration(Model model,
+			@ModelAttribute("product") Product product) {
 		return "product-registration";
 	}
 	
