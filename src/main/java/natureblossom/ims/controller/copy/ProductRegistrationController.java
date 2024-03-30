3ff449f0305cc31446012a3703cb4eb32c398d57
rkyzk <reikoyazaki@yahoo.com>
2024-03-30 13:47:22 +0900

    add AwsConfig

---
+++ src/main/java/natureblossom/ims/controller/copy/ProductRegistrationController.java
@@ -0,0 +1,30 @@
+package natureblossom.ims.controller.copy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import natureblossom.ims.entity.Product;
+
+/** Controller for product registration page.
+ *
+ * @author R.Yazaki
+ * @version 1.0.0
+ */
+@Controller
+public class ProductRegistrationController {
+	/**
+	 * Display product registration page.
+	 *
+	 * @param  model
+	 * @param  product
+	 * @return product registration page
+	 */
+	@GetMapping("/product-registration")
+	public String getProductRegistration(Model model,
+			@ModelAttribute("product") Product product) {
+		return "product-registration";
+	}
+	
+}
