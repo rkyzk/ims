d8ca20803b8c617a4d0bfefd2f169725cfb6f962
rkyzk <reikoyazaki@yahoo.com>
2024-04-06 14:16:23 +0900

    add dollar signs on reg & update pages

--- src/main/java/natureblossom/ims/controller/ProductListController.java
+++ src/main/java/natureblossom/ims/controller/ProductListController.java
@@ -1,14 +1,17 @@
 package natureblossom.ims.controller;
 
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import natureblossom.ims.entity.Product;
 import natureblossom.ims.service.ProductService;
@@ -24,6 +27,9 @@ public class ProductListController {
 	@Autowired
 	private ProductService productService;
 	
+	@Autowired
+	private MessageSource msg;
+	
 	@Value("${aws.endpoint.url}")
 	private String endpoint;
 	
@@ -35,7 +41,8 @@ public class ProductListController {
 	 * @return product list page
 	 */
 	@GetMapping("/product-list")
-	public String getProductList(Model model) {
+	public String getProductList(Model model, Locale locale,
+			RedirectAttributes redirectAttributes) {
 		List<Product> prodList = productService.getProductList();	
 		model.addAttribute("prodList", prodList);
 		model.addAttribute("itemCount", prodList.size());
@@ -44,8 +51,10 @@ public class ProductListController {
 	}
 	
 	@PostMapping("/delete")
-	public String delete(Model model, @RequestParam int id) {
-		int returnVal = productService.deleteProduct(id);	
+	public String delete(Model model, Locale locale, @RequestParam int id,
+			RedirectAttributes redirectAttributes) {
+		int returnVal = productService.deleteProduct(id);
+		redirectAttributes.addFlashAttribute("message", msg.getMessage("DELSUC", null, locale));
 		return "redirect:/product-list";
 	}
 }
